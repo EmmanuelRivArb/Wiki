140f85ddc8c2ae2c1d0d409ca8e182efc95c9a27
Emmanuel Rivero <emmanuel_cesman@hotmail.com>
2023-05-20 20:29:00 +0100

    Colocando nombre a la BD de roles

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,6 +1,7 @@
 import { ObjectType, Field, ID } from '@nestjs/graphql';
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity({ name: 'roles' })
 @ObjectType()
 export class Role {
   @PrimaryGeneratedColumn('uuid')
